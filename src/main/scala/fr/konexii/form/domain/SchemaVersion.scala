272ee94f57d72f446cda60235f98766e4a1e1497
Arthur Soulié <arthur.soulie@icloud.com>
2024-12-15 13:49:50 +0100

    Handle side effects when getting current date time from system
    This commit handles side effects from getting a LocalDateTime from the
    host system. The code could look better but I was stuck for now. See
    https://discord.com/channels/632277896739946517/632278585700384799/1317833443564064802
    for more informations and updates.

--- src/main/scala/fr/konexii/form/domain/SchemaVersion.scala
+++ src/main/scala/fr/konexii/form/domain/SchemaVersion.scala
@@ -1,15 +1,16 @@
 package fr.konexii.form
 package domain
 
+import cats.ApplicativeThrow
 import cats.effect.kernel.Clock
-import cats.Functor
-import cats.syntax.functor._
+import cats.MonadThrow
+import java.time.DateTimeException
+import java.time.Instant
 import java.time.LocalDateTime
+import java.time.ZoneOffset
 import scala.concurrent.duration.FiniteDuration
 
 import fr.konexii.form.domain.Block
-import java.time.Instant
-import java.time.ZoneId
 
 final case class SchemaVersion(
     date: LocalDateTime,
@@ -18,15 +19,23 @@ final case class SchemaVersion(
 
 object SchemaVersion {
 
+  /* Cannot do better for now, see https://discord.com/channels/632277896739946517/632278585700384799/1317833443564064802*/
+
   def apply[F[_]](
       content: Entity[Block]
-  )(implicit G: Clock[F], F: Functor[F]): F[SchemaVersion] =
-    F.map(G.realTime)((fd: FiniteDuration) =>
-      SchemaVersion(
-        LocalDateTime
-          .ofInstant(Instant.ofEpochMilli(fd.toMillis), ZoneId.systemDefault()),
-        content
+  )(implicit G: Clock[F], F: MonadThrow[F]): F[SchemaVersion] = {
+    val localDateTime: F[LocalDateTime] =
+      F.flatMap(G.realTime)((fd: FiniteDuration) =>
+        localDateTimeFromMilis(fd.toMillis)
       )
+    F.map(localDateTime)(SchemaVersion(_, content))
+  }
+
+  private def localDateTimeFromMilis[F[_]](
+      milis: Long
+  )(implicit F: MonadThrow[F]): F[LocalDateTime] =
+    F.catchOnly[DateTimeException](
+      LocalDateTime.ofInstant(Instant.ofEpochMilli(milis), ZoneOffset.UTC)
     )
 
 }
